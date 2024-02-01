41e8012829de2fa9f7f89cbbc1080033fe9d9216
vs-ramya <sramya8051@gmail.com>
2024-02-01 14:25:10 +0530

    Added header to the website

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+import Button from "./Button";
+import BannerImage from '../../Assets/Banner.jpg';
+
+
+import  classes from './Header.module.css';
+const Header=((props)=> {
+    return(
+        <Fragment>
+            <header className={classes.header}>
+                <h1>Meals of the day!!</h1>
+                <Button/>
+                </header>
+                <div className={classes['main-image']}>
+                    <img src={BannerImage} alt="delicious foods"/>
+                </div>
+            
+        </Fragment>
+
+    )
+
+})
+export default Header;
